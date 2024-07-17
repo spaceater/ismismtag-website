38c8bff2283f1ab06af09f7d23d5846d45625c5d
zhao1142 <122301664+spaceater@users.noreply.github.com>
2024-07-17 22:37:41 +0800

    修改Bug，优化显示效果

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ xhr.onload = ()=>{
             ism_node.addEventListener("click",function(){pinISM(ism_node)})
             document.getElementById("search_button").addEventListener("click",function(){searchISM(document.getElementById("search_box").value)})
             document.getElementById("clear_search_button").addEventListener("click",function(){searchISM(""),document.getElementById("search_box").value=""})
-            document.getElementById("axis_label").addEventListener("mouseenter",function(){showIntroduction()})
             showIntroduction()
         }
     }
@@ -60,7 +59,7 @@ function unselectISM(ism_node){
         setISMInfo(ism_pinned)
     }
     else{
-        removeISMInfo()
+        showIntroduction()
     }
     let ism_tag = ism_node.getAttribute("ism_tag")
     let ism_length=ism_tag.length
@@ -78,65 +77,15 @@ function unselectISM(ism_node){
 
 function setISMInfo(ism_node){
     let ism_tag = ism_node.getAttribute("ism_tag")
-    document.getElementById("ism_name").textContent = ism_tag + '\n' + ism_data[ism_tag]["ch_name"] + '\n' + ism_data[ism_tag]["en_name"]
-    let axis_list_length = ism_data[ism_tag]["axis_list"].length
-    document.getElementById("ism_axis").textContent = ""
-    for(let i=0;i<axis_list_length;i++){
-        document.getElementById("ism_axis").textContent += ism_data[ism_tag]["axis_list"][i] + '\n' 
-    }
-    let feature_list_length = ism_data[ism_tag]["feature_list"].length
-    document.getElementById("ism_features").textContent = ""
-    for(let i=0;i<feature_list_length;i++){
-        document.getElementById("ism_features").textContent += ism_data[ism_tag]["feature_list"][i] + '\n'
-    }
-    document.getElementById("ism_counterpart").textContent = ism_data[ism_tag]["figure"] + '\n' + ism_data[ism_tag]["guise"] + '\n' + ism_data[ism_tag]["group"]
-    document.getElementById("ism_link").textContent = ism_data[ism_tag]["link"]
-    document.getElementById("ism_link").innerHTML = document.getElementById("ism_link").innerHTML.replace(ism_data[ism_tag]["link"].split('：')[1],"<span style='text-decoration:underline'>$&</span>")
-    document.getElementById("ism_link").href = ism_data[ism_tag]["link"].split('：')[1]
-    if(document.getElementById("search_box").value!="")
-        renewInfo(document.getElementById("search_box").value)
-}
-
-function selectISM(ism_node){    
-    setISMInfo(ism_node)
-    let ism_tag = ism_node.getAttribute("ism_tag")
-    let ism_length=ism_tag.length
-    let ism_data_list_length = ism_detial_list.length
-    for(let i=0;i<ism_data_list_length;i++){
-        let ism_node = ism_detial_list[i]
-        if(ism_node.getAttribute("ism_tag").slice(0,ism_length) == ism_tag){
-            ism_node.classList.add("selected")
-            if(ism_node.getAttribute("ism_tag").length==3){
-                document.getElementById(ism_tag).classList.add("selected")
-            }
-        }
-    }
-}
-
-function unselectISM(ism_node){
-    if(ism_pinned != null){
-        setISMInfo(ism_pinned)
-    }
-    else{
-        removeISMInfo()
-    }
-    let ism_tag = ism_node.getAttribute("ism_tag")
-    let ism_length=ism_tag.length
-    let ism_data_list_length = ism_detial_list.length
-    for(let i=0;i<ism_data_list_length;i++){
-        let ism_node = ism_detial_list[i]
-        if(ism_node.getAttribute("ism_tag").slice(0,ism_length) == ism_tag){
-            ism_node.classList.remove("selected")
-            if(ism_node.getAttribute("ism_tag").length==3){
-                document.getElementById(ism_tag).classList.remove("selected")
-            }
-        }
-    }
-}
-
-function setISMInfo(ism_node){
-    let ism_tag = ism_node.getAttribute("ism_tag")
-    document.getElementById("ism_name").textContent = ism_tag + '\n' + ism_data[ism_tag]["ch_name"] + '\n' + ism_data[ism_tag]["en_name"]
+    if(ism_tag.length>=1)
+        document.getElementById("ism_name").innerHTML = "<span style='color:red;border:solid black 1px'>"+ism_tag[0]+"</span>"
+    if(ism_tag.length>=3)
+        document.getElementById("ism_name").innerHTML += "-<span style='color:green;border:solid black 1px'>"+ism_tag[2]+"</span>"
+    if(ism_tag.length>=5)
+        document.getElementById("ism_name").innerHTML += "-<span style='color:blue;border:solid black 1px'>"+ism_tag[4]+"</span>"
+    if(ism_tag.length>=7)
+        document.getElementById("ism_name").innerHTML += "-<span style='color:darkorange;border:solid black 1px'>"+ism_tag[6]+"</span>"
+    document.getElementById("ism_name").innerHTML += '\n' + ism_data[ism_tag]["ch_name"] + '\n' + ism_data[ism_tag]["en_name"]
     let axis_list_length = ism_data[ism_tag]["axis_list"].length
     document.getElementById("ism_axis").textContent = ""
     for(let i=0;i<axis_list_length;i++){
@@ -155,7 +104,7 @@ function setISMInfo(ism_node){
     if(ism_data[ism_tag]["group"]!="")
         document.getElementById("ism_counterpart").textContent += ism_data[ism_tag]["group"] + '\n'
     document.getElementById("ism_link").textContent = ism_data[ism_tag]["link"]
-    document.getElementById("ism_link").innerHTML = document.getElementById("ism_link").innerHTML.replace(ism_data[ism_tag]["link"].split('：')[1],"<span style='text-decoration:underline'>$&</span>")
+    document.getElementById("ism_link").innerHTML = document.getElementById("ism_link").innerHTML.replace(ism_data[ism_tag]["link"].split('：')[1],"<span style='text-decoration:underline;'>$&</span>")
     document.getElementById("ism_link").href = ism_data[ism_tag]["link"].split('：')[1]
     if(document.getElementById("search_box").value!="")
         renewInfo(document.getElementById("search_box").value)
@@ -171,10 +120,10 @@ function removeISMInfo(){
 
 function renewInfo(target){
     let reg = new RegExp(target,'gi')//全局且忽略大小写
-    document.getElementById("ism_name").innerHTML = document.getElementById("ism_name").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39)'>$&</span>")
-    document.getElementById("ism_axis").innerHTML = document.getElementById("ism_axis").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39)'>$&</span>")
-    document.getElementById("ism_features").innerHTML = document.getElementById("ism_features").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39)'>$&</span>")
-    document.getElementById("ism_counterpart").innerHTML = document.getElementById("ism_counterpart").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39)'>$&</span>")
+    document.getElementById("ism_name").innerHTML = document.getElementById("ism_name").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39);'>$&</span>")
+    document.getElementById("ism_axis").innerHTML = document.getElementById("ism_axis").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39);'>$&</span>")
+    document.getElementById("ism_features").innerHTML = document.getElementById("ism_features").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39);'>$&</span>")
+    document.getElementById("ism_counterpart").innerHTML = document.getElementById("ism_counterpart").innerHTML.replace(reg,"<span style='background-color:rgb(225, 172, 39);'>$&</span>")
 }
 
 function pinISM(ism_node){
@@ -251,27 +200,25 @@ function searchISM(target){
                     }
                 }
             }
-        }
-        document.getElementById("ism_name").textContent = "检索完成"
+        } 
     }
+    showIntroduction()
 }
 
 function showIntroduction(){
-    searchISM("")
-    document.getElementById("search_box").value=""
-    document.getElementById("ism_name").textContent = ism_data["introduction"].ch_name + '\n' + ism_data["introduction"].en_name
-    let introduction_length = ism_data["introduction"]["axis_list"].length
-    document.getElementById("ism_axis").textContent = ""
+    document.getElementById("ism_name").textContent = ism_data["introduction"].ismismcube
+    let introduction_length = ism_data["introduction"]["ismismcube_introduction"].length
+    document.getElementById("ism_axis").textContent = '\t'
     for(let i=0;i<introduction_length;i++){
-        document.getElementById("ism_axis").textContent += ism_data["introduction"]["axis_list"][i] + '\n'
+        document.getElementById("ism_axis").textContent += ism_data["introduction"]["ismismcube_introduction"][i]
     }
-    introduction_length = ism_data["introduction"]["feature_list"].length
-    document.getElementById("ism_features").textContent = ""
+    document.getElementById("ism_features").textContent = ism_data["introduction"].ismism
+    introduction_length = ism_data["introduction"]["ismism_introduction"].length
+    document.getElementById("ism_counterpart").textContent = '\t'
     for(let i=0;i<introduction_length;i++){
-        document.getElementById("ism_features").textContent += ism_data["introduction"]["feature_list"][i] + '\n'
+        document.getElementById("ism_counterpart").textContent += ism_data["introduction"]["ismism_introduction"][i]
     }
-    document.getElementById("ism_counterpart").textContent = ism_data["introduction"]["guise"]
-    document.getElementById("ism_link").textContent = ism_data["introduction"]["group"] + '\n' + ism_data["introduction"]["link"]
-    document.getElementById("ism_link").innerHTML = document.getElementById("ism_link").innerHTML.replace(ism_data["introduction"]["link"].split('：')[1],"<span style='text-decoration:underline'>$&</span>")
+    document.getElementById("ism_link").textContent = ism_data["introduction"]["warning"] + '\n' + ism_data["introduction"]["group"] + '\n' + ism_data["introduction"]["link"]
+    document.getElementById("ism_link").innerHTML = document.getElementById("ism_link").innerHTML.replace(ism_data["introduction"]["link"].split('：')[1],"<span style='text-decoration:underline;'>$&</span>")
     document.getElementById("ism_link").href = ism_data["introduction"]["link"].split('：')[1]
 }
